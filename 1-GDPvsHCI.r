b186e73f96a4162d3ed2f9561fca10e3debd00b2
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-03-22 00:48:51 +0000

    Update 1-GDPvsHCI.r

--- 1-GDPvsHCI.r
+++ 1-GDPvsHCI.r
@@ -3,16 +3,12 @@ theme_set(theme_gray())
 
 url <- 'https://web.tecnico.ulisboa.pt/~paulo.soares/pe/projeto/Paises_PIB_ICH.csv'
 data <- read.csv(url, check.names = FALSE)
-
 continents <- c("Asia", "Africa")
 countries <- c("United Arab Emirates", "Nepal", "Comoros", "Namibia")
 
-data_continent <- subset(data, Continent %in% continents)
-data_countries <- subset(data, Country %in% countries)
-
-ggplot(data_continent, aes(x = GDP, y = HCI, color = Continent)) +
+ggplot(subset(data, Continent %in% continents), aes(x = GDP, y = HCI, color = Continent)) +
   geom_point() +
-  geom_text(data = data_countries, aes(label = Country), hjust = 0, vjust = 0, size = 3) +
+  geom_text(data = subset(data, Country %in% countries), aes(label = Country), hjust = 0, vjust = 0, size = 3) +
   scale_x_log10() +
   labs(title = "HCI - GDP per capita",
        x = "GDP per capita (international dollars)",
