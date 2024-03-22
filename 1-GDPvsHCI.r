2920175ddc82e1c24b697cc819ce8cee7d47c1cf
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-03-22 00:18:29 +0000

    Update 1-GDPvsHCI.r

--- 1-GDPvsHCI.r
+++ 1-GDPvsHCI.r
@@ -1,22 +1,19 @@
-
 library(ggplot2)
+
 theme_set(theme_gray())
 
 url <- 'https://web.tecnico.ulisboa.pt/~paulo.soares/pe/projeto/Paises_PIB_ICH.csv'
 data <- read.csv(url, check.names = FALSE)
+
 continents <- c("Asia", "Africa")
 countries <- c("United Arab Emirates", "Nepal", "Comoros", "Namibia")
 
-data_subset <- data[data$Continent %in% continents & data$Country %in% countries,]
+data_continent <- data[data$Continent %in% continents, ]
 
-data |>
-  subset(Continent %in% continents) |>
-  ggplot() +
-  geom_point(aes(`GDP`, `HCI`, color = Continent)) +
+ggplot(data_continent, aes(x = GDP, y = HCI, color = Continent)) +
+  geom_point() +
+  geom_text(data = data[data$Country %in% countries,], aes(label = Country), hjust = 0, vjust = 0, size = 3) +
   scale_x_log10() +
-  labs(title = paste("Life expectancy vs GDP per capita in"),
+  labs(title = "HCI vs GDP per capita",
        x = "GDP per capita (international dollars)",
        y = "HCI (0-1) data from 2020")
-  subset(Country %in% countries) |>
-  geom_text()
-
