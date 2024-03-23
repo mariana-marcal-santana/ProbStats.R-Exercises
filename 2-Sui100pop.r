49d5c0656d98cf0145c9182818b22840d3fb3e93
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-03-23 16:50:05 +0000

    Update 2-Sui100pop.r

--- 2-Sui100pop.r
+++ 2-Sui100pop.r
@@ -4,4 +4,10 @@ url <- 'https://web.tecnico.ulisboa.pt/~paulo.soares/pe/projeto/master.csv'
 data <- read.csv(url)
 data <- subset(data, year == 1986 & age == "25-34 years")
 
-sexes <- c("female", "male")
+#sexes <- c("female", "male")
+#data <- subset(data, sex %in% sexes)
+
+ggplot(data) +
+  geom_boxplot(aes(sex, suicides.100k.pop)) +
+  labs(title = paste0("Nº of suicides 100k habitants (1986/25-34 years)"), 
+       y = "Suicides", x = "Sex")
