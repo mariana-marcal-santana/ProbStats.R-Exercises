16352ffa41c9fcffeda5221c01b3b3c8cb2f651a
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-04-16 21:43:52 +0100

    Create 6-ECompGammaDist.r

---
+++ 6-ECompGammaDist.r
@@ -0,0 +1,15 @@
+a <- 4
+n_samples <- 1000
+n <- 40
+
+set.seed(1973)
+
+exact_value <- 1 - pgamma(126, shape = 40, scale = 1/4) # 1 - P(Y <= 126)
+
+amostras <- matrix(rexp(n_samples * n, rate = 1/4), nrow = n_samples)
+
+somas <- rowSums(amostras)
+
+prob_maior_126 <- mean(somas > 126)
+
+result <- round(abs(exact_value - prob_maior_126) * 100, 4)
