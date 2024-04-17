a6abfda186bc90eaa800669e25c3ab42f1aeb281
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-04-17 19:28:14 +0100

    sub scale por rate em pgamma

--- 6-ECompGammaDist.r
+++ 6-ECompGammaDist.r
@@ -4,7 +4,7 @@ n <- 40
 
 set.seed(1973)
 
-exact_value <- 1 - pgamma(126, shape = 40, scale = 1/4) # 1 - P(Y <= 126)
+exact_value <- 1 - pgamma(126, shape = 40, rate = 1/4) # 1 - P(Y <= 126)
 
 amostras <- matrix(rexp(n_samples * n, rate = 1/4), nrow = n_samples)
 
@@ -12,4 +12,4 @@ somas <- rowSums(amostras)
 
 prob_maior_126 <- mean(somas > 126)
 
-result <- round(abs(exact_value - prob_maior_126) * 100, 4)
+result <- round(abs(exact_value - prob_maior_126) * 100, 4) # 0.6141
