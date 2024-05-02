b6ccf4c34252742ef424574fd6feaa4a310f67bc
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-05-02 11:52:14 +0100

    Update 1-GDPvsHCI.r

--- 1-GDPvsHCI.r
+++ 1-GDPvsHCI.r
@@ -1,5 +1,4 @@
 library(ggplot2)
-theme_set(theme_gray())
 
 url <- 'https://web.tecnico.ulisboa.pt/~paulo.soares/pe/projeto/Paises_PIB_ICH.csv'
 data <- read.csv(url, check.names = FALSE)
@@ -8,8 +7,10 @@ countries <- c("United Arab Emirates", "Nepal", "Comoros", "Namibia")
 
 ggplot(subset(data, Continent %in% continents), aes(x = GDP, y = HCI, color = Continent)) +
   geom_point() +
-  geom_text(data = subset(data, Country %in% countries), aes(label = Country), hjust = 0, vjust = 0, size = 3, show.legend = FALSE) +
+  geom_point(data = subset(data, Country %in% countries), size = 3) +
+  geom_text(data = subset(data, Country %in% countries), aes(label = Country), hjust = 1.1, vjust = -0.5, size = 3.5, show.legend = FALSE) +
   scale_x_log10() +
-  labs(title = "HCI - GDP per capita",
-       x = "GDP per capita (international dollars)",
-       y = "HCI (0-1) data from 2020")
+  labs(title = "Human Capital Index x Gross Domestic Product per capita",
+       x = "Gross Domestic Product per capita (international dollars)",
+       y = "Human Capital Index in 2020 (0-1)") +
+  theme(plot.title = element_text(hjust = 0.5))  
