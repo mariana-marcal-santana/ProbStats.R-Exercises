47f419deefe8165c2bddf8614013357a81af5332
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-05-20 20:19:54 +0100

    Update 3-ElectricityProportions.r

--- 3-ElectricityProportions.r
+++ 3-ElectricityProportions.r
@@ -1,17 +1,15 @@
+library(readxl)
 library(ggplot2)
 
 data <- readxl::read_xlsx("electricity.xlsx")
-data <- subset(data, YEAR >= 2015 & PRODUCT == "Renewables")
+data <- subset(data, YEAR >= 2015 & COUNTRY %in% c("IEA Total", "Italy", "Latvia") & PRODUCT == "Renewables")
+
 data$share <- as.numeric(data$share)
-#data$TIME <- as.factor(data$TIME)
+data$DATE <- as.Date(paste(data$YEAR, data$MONTH, '01', sep="-"))
 
-countries <- c("IEA Total", "Italy", "Latvia")
-data <- subset(data, COUNTRY %in% countries)
-
-ggplot(data, aes(x = YEAR, y = share, color = COUNTRY)) +
-  geom_line() +
+ggplot(data, aes(x = DATE, y = share, color = COUNTRY)) +
+  geom_line(size = 0.75) +
   scale_y_continuous(labels = scales::percent_format()) +
   labs(title = "Monthly Proportion of Renewable Energy Production (2015-2022)",
        x = "Months",
        y = "Renewable Energy Proportion (%)")
-
