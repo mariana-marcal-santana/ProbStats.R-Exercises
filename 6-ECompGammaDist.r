23d2ff36ce943d6ac7d02611243b61655e2d78ff
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-06-05 17:29:20 +0100

    Update 6-ECompGammaDist.r

--- 6-ECompGammaDist.r
+++ 6-ECompGammaDist.r
@@ -1,43 +1,13 @@
-
 a <- 4
-n_samples <- 1000
 n <- 40
-
+n_samples <- 1000
 set.seed(1973)
 
-exact_value <- 1 - pgamma(126, shape = 40, rate = 1/4) # 1 - P(Y <= 126)
+exact_value <- 1 - pgamma(126, shape = n, rate = 1/a) # 1 - P(Y <= 126)
 
-#amostras <- 
-
-#amostras <- matrix(rexp(n_samples, rate = 1/4), nrow = n)
-
-amostras <- replicate(1000, rexp(n, 1/4))
+amostras <- replicate(n_samples, rexp(n, 1/a))
 
 somas <- colSums(amostras)
-
-prob_y_maior <- sum(somas > 126) / length(somas)
-
-
-
 prob_maior_126 <- mean(somas > 126)
 
-result <- round(abs(exact_value - prob_y_maior) * 100, 4) # 0.6141
-
-
-#########################################
-
-a <- 4
-n_samples <- 1000
-n <- 40
-
-set.seed(1973)
-
-exact_value <- 1 - pgamma(126, shape = 40, rate = 1/4) # 1 - P(Y <= 126)
-
-amostras <- matrix(rexp(n_samples * n, rate = 1/4), nrow = n_samples)
-
-somas <- rowSums(amostras)
-
-prob_maior_126 <- mean(somas > 126)
-
-result <- round(abs(exact_value - prob_maior_126) * 100, 4) # 0.6141
+result <- round(abs(exact_value - prob_maior_126) * 100, 4) # 0.1859
