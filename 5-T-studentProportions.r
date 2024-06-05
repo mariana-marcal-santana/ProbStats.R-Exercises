fe5f2d3bf4c8da27e333790709e824f9ee38f810
Mariana Santana <121701463+mariana-marcal-santana@users.noreply.github.com>
2024-06-05 17:21:48 +0100

    Update 5-T-studentProportions.r

--- 5-T-studentProportions.r
+++ 5-T-studentProportions.r
@@ -1,24 +1,23 @@
 n <- 23
 r <- 300
 m <- 170
-
 set.seed(1950)
 
-p <- pt(1.5, df = n) # t-student distribution with n degrees of freedom up to 1.5
-
-prop_samples <- numeric(r) # numeric vector with r spaces
+samples <- matrix(0, nrow=r, ncol=m)
 
 for (i in 1:r) {
-  
-  Zs <- rnorm(n + 1) # n+1 random numbers from a normal distribution (from 1 to n+1)
-  
-  T <- sqrt(n) * Zs[1] / sqrt(sum(Zs[-1]^2)) # t-student distribution
-  
-  prop_samples[i] <- mean(T <= 1.5) # calculates the mean of the t-student distribution's values if they´re less than 1.5
+  for (j in 1:m) {
+    
+    Z <- rnorm(n + 1)
+    T <- sqrt(n) * Z[1] / sqrt(sum(Z[-1]^2))
+    samples[i, j] <- T
+    
+  }
 }
 
-mean_prop_samples <- mean(prop_samples) # mean of the prop_samples vector
+proportions <- rowMeans(samples <= 1.5)
+aprox_prob <- mean(proportions)
 
-dif <- abs(mean_prop_samples - p) # difference between the mean of the prop_samples vector and the p value
+exact_prob <- pt(1.5, df=n)
 
-result <- round(dif * 100, 5) # 2.36087
+result <- round(abs(aprox_prob - exact_prob) * 100, 5) # 0.06266
